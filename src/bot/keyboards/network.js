f62aa831ac58cc0b9499fa79fb3269b9be2e5484
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-09-02 23:23:07 +0700

    Update network.js

--- src/bot/keyboards/network.js
+++ src/bot/keyboards/network.js
@@ -6,7 +6,6 @@ const urlapi = require('url');
 const settings = require('../../../settings');
 const cfg = require('../../config');
 const stringToBoolean = require('../../lib/string-to-boolean');
-const cfg = require('../../config');
 
 
 
